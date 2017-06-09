449912cab3f790e55509584b301587241c836140
김유빈 <1rladbqls@gmail.com>
2017-06-09 22:41:38 +0900

    Revert "1. 0번 상자의 좌표값 초기값을 0으로 해놓음"
    This reverts commit 5ea9cc806f86536a13ec5d651ed4d5a79a3f9c04.

--- JavaProject/src/yubinConpo/componetVer.java
+++ JavaProject/src/yubinConpo/componetVer.java
@@ -60,8 +60,8 @@ public class componetVer extends JPanel
 		//마우스 리스너 등록
 		
 		//박스0. 생성. 아무것도 안 가리킬때 사용하는 Null 상자
-		startV.add(new Point(0,0));
-		endV.add(new Point(0,0));
+		startV.add(new Point(10,10));
+		endV.add(new Point(10,10));
 		clickV.add(false);
 		TypeV.add(null);
 		TextV.add(null);
@@ -114,18 +114,15 @@ public class componetVer extends JPanel
 			j.setSize(boxM.getRecWidth(i), boxM.getRecHeight(i)); 
 			j.setEnabled(true);
 			j.setFocusable(true);
-
+			
 			if(boxM.getClick(i))
 				j.setBackground(Color.RED);
 			else
 				j.setBackground(Color.WHITE);
 			
 			buttons.add(j);
-		}
-		for(int i=boxM.ArrSize()-1;i>0;i--)  // 모델 객체에 저장된 각 사각형을 매번 하나씩 전부 그림
 			add(buttons.get(i));
-
-		
+		}		
 		if(startP != null)
 		{
 			g.setColor(Color.BLACK);
@@ -192,51 +189,6 @@ public class componetVer extends JPanel
 			Rectangle tempRecSize; 
 			//사이즈 변경할 때 쓰는 임시 사각형
 
-			if(e.getButton()==1)//가운데 버튼 & 왼쪽 버튼으로 그릴 수 없게 막음
-			{
-				if(!ModeClick)//그리기 모드
-				{
-					TypeV.add(Choose); // 타입선택용
-					TextV.add(viewText); // 텍스트 입력용
-					
-					startP = e.getPoint();	 // 생성 시 그려질때 나오는 임시 사각형	
-
-					//현재 커서 좌표 저장 및 clickV에 해당 도형 번호 생성후 false로 초기화
-					startV.add(e.getPoint());
-					clickV.add(false);
-				}
-				else 		//선택모드
-				{
-					if(BoxNum!=0) //상자가 1개라도 있어야 작동함.(0번은 Null상자임으로 없는 셈 침)
-					{
-						tempRecSize = TransPoint.EndToTempRec(endV.get(BoxNum),15);
-						// 해당 사각형 끝점 주위에 임시 사각형(끝점 중심으로 가로 30, 세로 30)생성
-						// 임시 사각형(우측하단 모서리 근처) 안에 커서가 있을 경우
-						if(tempRecSize.contains(new Point(e.getX(),e.getY())))
-						{			
-							//드래그 시작을 표시
-							sizeDragged = true;
-						}
-						else
-						{	
-							//임시 사각형(크기조절용) 안에 X && 사각형 내부에 커서가 있을 경우
-							//사각형 자체를 움직여야함으로, 편의를 위해 해당 사각형을 임시사각형에 저장해서 연산.
-							Rectangle tempRec = TransPoint.StartToTempOval(startV.get(BoxNum),30);
-							rec = TransPoint.pointToRec(startV.get(BoxNum),endV.get(BoxNum)); //임시사각형
-							if(tempRec.contains(new Point(e.getX(),e.getY())))
-							{
-								//상대 위치 저장
-								moveP.x=e.getX() - rec.x;
-								moveP.y=e.getY() - rec.y;
-						
-								//드래그 시작을 표시
-								moveDragged = true;
-							}
-						}
-					}
-				}
-			}
-			
 			if(e.getButton()==2) // 마우스 가운데 바튼 누르면 만들기 모드 <-> 선택모드 선택 가능
 			{
 				if(ModeClick)
@@ -247,27 +199,64 @@ public class componetVer extends JPanel
 			else{}//구분용 신경 ㄴㄴ
 
 
+			if(!ModeClick)//그리기 모드
+			{
+				TypeV.add(Choose); // 타입선택용
+				TextV.add(viewText); // 텍스트 입력용
+				
+				startP = e.getPoint();	 // 생성 시 그려질때 나오는 임시 사각형	
+
+				//현재 커서 좌표 저장 및 clickV에 해당 도형 번호 생성후 false로 초기화
+				startV.add(e.getPoint());
+				clickV.add(false);
+			}
+			else 		//선택모드
+			{
+				if(BoxNum!=0) //상자가 1개라도 있어야 작동함.(0번은 Null상자임으로 없는 셈 침)
+				{
+					tempRecSize = TransPoint.EndToTempRec(endV.get(BoxNum),15);
+					// 해당 사각형 끝점 주위에 임시 사각형(끝점 중심으로 가로 30, 세로 30)생성
+					// 임시 사각형(우측하단 모서리 근처) 안에 커서가 있을 경우
+					if(tempRecSize.contains(new Point(e.getX(),e.getY())))
+					{			
+						//드래그 시작을 표시
+						sizeDragged = true;
+					}
+					else
+					{	
+						//임시 사각형(크기조절용) 안에 X && 사각형 내부에 커서가 있을 경우
+						//사각형 자체를 움직여야함으로, 편의를 위해 해당 사각형을 임시사각형에 저장해서 연산.
+						Rectangle tempRec = TransPoint.StartToTempOval(startV.get(BoxNum),30);
+						rec = TransPoint.pointToRec(startV.get(BoxNum),endV.get(BoxNum)); //임시사각형
+						if(tempRec.contains(new Point(e.getX(),e.getY())))
+						{
+							//상대 위치 저장
+							moveP.x=e.getX() - rec.x;
+							moveP.y=e.getY() - rec.y;
+					
+							//드래그 시작을 표시
+							moveDragged = true;
+						}
+					}
+				}
+			}
 		}
 
 
 		public void mouseReleased(MouseEvent e)
 		{
-			if(e.getButton()==1)
+			startP = null;
+			endP = null;// 릴리스하면 생성 시 그려질때 나오는 임시 사각형 삭제
+
+			if(!ModeClick) // 그리기 모드, 커서 떼는 순간 현재 좌표를 end에 넣음
 			{
-				startP = null;
-				endP = null;// 릴리스하면 생성 시 그려질때 나오는 임시 사각형 삭제
+				endV.add(e.getPoint());
+			}
+			//마우스 버튼이 릴리즈되면 드래그 모드 종료
+			sizeDragged = false;
+			moveDragged = false;	
 
-				if(!ModeClick) // 그리기 모드, 커서 떼는 순간 현재 좌표를 end에 넣음
-				{
-					endV.add(e.getPoint());
-				}
-				//마우스 버튼이 릴리즈되면 드래그 모드 종료
-				sizeDragged = false;
-				moveDragged = false;	
-
-			}	
-			repaint();		
-
+			repaint();			
 		}
 		
 		public void mouseDragged(MouseEvent e)
